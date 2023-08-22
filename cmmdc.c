e1e56e6a72be4a09cf16c6bd6b76cd19c73ee008
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-08-22 12:39:59 +0300

    Update cmmdc.c

--- cmmdc.c
+++ cmmdc.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main()
+{
+	unsigned a, b, rest;
+	FILE *fin, *fout;
+
+	fin = fopen("cmmdc.in", "r");
+
+	fscanf(fin, "%u", &a);
+	fscanf(fin, "%u", &b);
+
+	while (b != 0) {
+		rest = a % b;
+		a = b;
+		b = rest;
+	}
+	
+	fout = fopen("cmmdc.out", "w");
+	
+	a = a == 1 ? 0 : a;
+
+	fprintf(fout, "%u", a);
+
+	fclose(fin);
+	fclose(fout);
+
+	return 0;
+}
